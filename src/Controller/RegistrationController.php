96b040680452c51b907a4b9f3652f18372cb29ff
JarekDylewski <jaroslaw.dylewski@interia.pl>
2018-08-29 23:34:15 +0200

    Wydzielenie rejestracji do osobnej usługi

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,37 +3,46 @@
 namespace App\Controller;
 
 
-use App\Entity\User;
-use App\Form\UserType;
+use App\User\UserManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class RegistrationController extends AbstractController
 {
+    private $userManager;
+
+    public function __construct(UserManager $userManager)
+    {
+        $this->userManager = $userManager;
+    }
+
     /**
      * @Route("/register", name="user_registration")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    public function register(Request $request)
     {
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
+        $form = $this->userManager->registerUser($request);
 
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
-            $user->setPassword($password);
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-        };
+//        $user = new User();
+//        $form = $this->createForm(UserType::class, $user);
+//
+//        $form->handleRequest($request);
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+//            $user->setPassword($password);
+//
+//            $entityManager = $this->getDoctrine()->getManager();
+//            $entityManager->persist($user);
+//            $entityManager->flush();
+//
+//        };
 
         return $this->render(
             'registration/register.html.twig',
-            array('form' => $form->createView())
+            array('form' => $form['form']->createView(),
+                'errors' => $form['errors']
+            )
         );
     }
 }
