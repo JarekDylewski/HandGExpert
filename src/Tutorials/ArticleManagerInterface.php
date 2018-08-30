cb458f35537cac27ad4224cd44f7767e0afcaeca
JarekDylewski <jaroslaw.dylewski@interia.pl>
2018-08-30 22:34:53 +0200

    nowa funkcja w interfejsie

--- src/Tutorials/ArticleManagerInterface.php
+++ src/Tutorials/ArticleManagerInterface.php
@@ -6,4 +6,6 @@ namespace App\Tutorials;
 interface ArticleManagerInterface
 {
     public function getPaginatedArticlesMenuList(int $page, $persistentObject): array;
+
+    public function addArticle($title, $category, $author, $imgUrl, $shortDescription, $mainContent, $persistentObject);
 }
