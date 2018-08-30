acf2554caa3dd273e44b02ede24c9ac9adc78eeb
JarekDylewski <jaroslaw.dylewski@interia.pl>
2018-08-30 22:35:50 +0200

    Wydzielenie dodawania artykułu do osobnej usługi

--- src/Tutorials/ArticleManager.php
+++ src/Tutorials/ArticleManager.php
@@ -3,19 +3,23 @@
 namespace App\Tutorials;
 
 
+use App\Entity\TutorialArticle;
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ArticleManager implements ArticleManagerInterface
 {
     private $doctrineManager;
+    private $validator;
 
     private $allTutorialsArticleID;
     private $listOfPagesIdToFind;
     private $countPagination;
 
-    public function __construct(ManagerRegistry $doctrineManager)
+    public function __construct(ManagerRegistry $doctrineManager, ValidatorInterface $validator)
     {
         $this->doctrineManager = $doctrineManager;
+        $this->validator = $validator;
     }
 
     public function getPaginatedArticlesMenuList(int $page, $persistentObject): array
@@ -47,6 +51,54 @@ class ArticleManager implements ArticleManagerInterface
     public function getCountPagination()
     {
         return $this->countPagination;
+    }
+
+    public function addArticle($title, $category, $author, $imgUrl, $shortDescription, $mainContent, $persistentObject)
+    {
+        $currentDate = new \DateTime('now');
+
+        $tutorialEntity = new TutorialArticle();
+        $entityMenager = $this->doctrineManager->getManager();
+
+        $tutorialEntity->setTitle($title);
+        $tutorialEntity->setCategory($category);
+        $tutorialEntity->setAuthor($author);
+        $tutorialEntity->setCreationDate($currentDate);
+        $tutorialEntity->setURLImg($imgUrl);
+        $tutorialEntity->setShortDescription($shortDescription);
+        $tutorialEntity->setMainTopic($mainContent);
+
+        $validationErrors = $this->validator->validate($tutorialEntity);
+
+        if (count($validationErrors) > 0) {
+            $failureMessage = 'Something is wrong! Please complete the fields with <span style="color:red;font-size: 30px;">*';
+            return [
+                'message' => $failureMessage,
+                'option1' => 'Home',
+                'option2' => 'Back to editor',
+                'href1' => 'home',
+                'href2' => 'addNewArticle',
+                'errors' => $validationErrors,
+            ];
+        }
+
+        $entityMenager->persist($tutorialEntity);
+        $entityMenager->flush();
+        $succesMessage = 'Added successfully!';
+
+        $allTutorialsArticleID = $this->doctrineManager
+            ->getRepository($persistentObject)
+            ->select('id');
+        $lastTopic = count($allTutorialsArticleID);
+
+        return [
+            'message' => $succesMessage,
+            'option1' => 'Back to tutorials',
+            'option2' => 'Show topic',
+            'href1' => 'tutorials',
+            'href2' => 'tutorialShow',
+            'ID' => $lastTopic
+        ];
     }
 
 }
