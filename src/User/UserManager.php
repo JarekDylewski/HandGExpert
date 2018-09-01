65ab1d5b372fffbd1c0f0457f986e659ee37552d
JarekDylewski <jaroslaw.dylewski@interia.pl>
2018-09-01 23:48:46 +0200

    Pierwsze zmiany w systemie rejestracji - niedokonczone!

--- src/User/UserManager.php
+++ src/User/UserManager.php
@@ -18,51 +18,95 @@ class UserManager
 {
 
     private $userRepository;
-    private $managerRegistry;
+    private $doctrineManager;
     private $passwordEncoder;
     private $validator;
 
-    public function __construct(UserRepository $userRepository, UserPasswordEncoderInterface $passwordEncoder, ManagerRegistry $managerRegistry, ValidatorInterface $validator)
-    {
-        $this->managerRegistry = $managerRegistry;
+    public function __construct(
+        UserRepository $userRepository,
+        UserPasswordEncoderInterface $passwordEncoder,
+        ManagerRegistry $managerRegistry,
+        ValidatorInterface $validator
+    ) {
+        $this->doctrineManager = $managerRegistry;
         $this->userRepository = $userRepository;
         $this->passwordEncoder = $passwordEncoder;
         $this->validator = $validator;
     }
 
-    public function registerUser($password, $userName, $email)
+    public function registerUser($password, $repeatPassword, $userName, $email)
     {
-
         $user = new User();
-        $formFactory = Forms::createFormFactoryBuilder()
-            ->addExtension(new HttpFoundationExtension())
-            ->getFormFactory();
 
         $user->setUsername($userName);
         $user->setEmail($email);
         $user->setPlainPassword($password);
-        $form = $formFactory->create(UserType::class, $user);
 
         $validationErrors = $this->validator->validate($user);
+        if ($password !== $repeatPassword) {
+            $passwordError = 'passwords not identical';
+        }
+        if (count($validationErrors) > 0) {
 
-        if (isset($password) && isset($userName) && isset($email)) {
-            $form->submit('user');
+
+            return [
+                'errors' => $validationErrors,
+                'templateName' => 'registration/register.html.twig',
+                'pathToRouteWhereFormSend' => 'user_registration',
+                'message' => 'Woops! Something is wrong!',
+                'passwordError' => $passwordError
+            ];
+        } else {
+            if ($validationErrors === 0) {
+                $password = $this->passwordEncoder->encodePassword($user, $user->getPlainPassword());
+                $user->setPassword($password);
+
+                $entityManager = $this->doctrineManager->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                return ['message' => 'Registration success!, check your email to activate confirmation link!'];
+            }
         }
 
-        if ($form->isSubmitted() && count($validationErrors) === 0) {
-            $password = $this->passwordEncoder->encodePassword($user, $user->getPlainPassword());
-            $user->setPassword($password);
 
-            $entityManager = $this->managerRegistry->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
+//        $dispatcher = new EventDispatcher();
+//        $event = new UserRegisteredEvent($form);
+//        $dispatcher->dispatch(UserRegisteredEvent::NAME, $event);
 
-        };
+        //return ['form' => $form, 'errors' => $validationErrors];
 
-        $dispatcher = new EventDispatcher();
-        $event = new UserRegisteredEvent($form);
-        $dispatcher->dispatch(UserRegisteredEvent::NAME, $event);
 
-        return ['form' => $form, 'errors' => $validationErrors];
+//        $user = new User();
+//        $formFactory = Forms::createFormFactoryBuilder()
+//            ->addExtension(new HttpFoundationExtension())
+//            ->getFormFactory();
+//
+//        $user->setUsername($userName);
+//        $user->setEmail($email);
+//        $user->setPlainPassword($password);
+//        $form = $formFactory->create(UserType::class, $user);
+//
+//        $validationErrors = $this->validator->validate($user);
+//
+//        if (isset($password) && isset($userName) && isset($email)) {
+//            $form->submit('user');
+//        }
+//
+//        if ($form->isSubmitted() && count($validationErrors) === 0) {
+//            $password = $this->passwordEncoder->encodePassword($user, $user->getPlainPassword());
+//            $user->setPassword($password);
+//
+//            $entityManager = $this->managerRegistry->getManager();
+//            $entityManager->persist($user);
+//            $entityManager->flush();
+//
+//        };
+//
+//        $dispatcher = new EventDispatcher();
+//        $event = new UserRegisteredEvent($form);
+//        $dispatcher->dispatch(UserRegisteredEvent::NAME, $event);
+//
+//        return ['form' => $form, 'errors' => $validationErrors];
     }
 }
