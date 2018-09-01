b781f5815342065a7c5f08670ddce9aefebdc18e
JarekDylewski <jaroslaw.dylewski@interia.pl>
2018-09-01 23:48:37 +0200

    Pierwsze zmiany w systemie rejestracji - niedokonczone!

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -22,19 +22,50 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request)
     {
-        //TODO przerobić system rejestrowania - łącznie z widokiem
-        $form = $this->userManager->registerUser(
-            $request->get('user')['plainPassword']['first'],
-            $request->get('user')['userName'],
-            $request->get('user')['email']
-        );
 
-
-        return $this->render(
-            'registration/register.html.twig',
-            array('form' => $form['form']->createView(),
-                'errors' => $form['errors']
-            )
+        $registration = $this->userManager->registerUser(
+            $request->get('userName'),
+            $request->get('email'),
+            $request->get('plainPassword')['first'],
+            $request->get('plainPassword')['second']
         );
+        dump($registration);
+        return $this->render($registration['templateName'], $registration);
+//        //TODO przerobić system rejestrowania - łącznie z widokiem
+//        $form = $this->userManager->registerUser(
+//            $request->get('user')['plainPassword']['first'],
+//            $request->get('user')['userName'],
+//            $request->get('user')['email']
+//        );
+        //$form['form']->handleRequest($request);
+
+//        $user = new User();
+//        $form = $this->createForm(UserType::class, $user);
+//
+//        $form->handleRequest($request);
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+//            $user->setPassword($password);
+//
+//            $entityManager = $this->getDoctrine()->getManager();
+//            $entityManager->persist($user);
+//            $entityManager->flush();
+//
+//        };
+
+//        return $this->render(
+//            'registration/register.html.twig',
+//            array('form' => $form['form']->createView(),
+//                'errors' => $form['errors']
+//            )
+//        );
+    }
+
+    /**
+     * @Route("/registrationForm", name="registration_form")
+     */
+    public function registrationFormulage()
+    {
+        return $this->render('registration/register.html.twig', ['pathToRouteWhereFormSend' => 'user_registration']);
     }
 }
