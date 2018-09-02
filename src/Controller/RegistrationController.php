f836886535affc8638db64a59fce6d16ea00fb30
JarekDylewski <jaroslaw.dylewski@interia.pl>
2018-09-02 22:59:13 +0200

    Poprawienie drobnego błędu - kolejność argumentów

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -22,43 +22,14 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request)
     {
-
         $registration = $this->userManager->registerUser(
-            $request->get('userName'),
-            $request->get('email'),
             $request->get('plainPassword')['first'],
-            $request->get('plainPassword')['second']
+            $request->get('plainPassword')['second'],
+            $request->get('userName'),
+            $request->get('email')
         );
-        dump($registration);
+
         return $this->render($registration['templateName'], $registration);
-//        //TODO przerobić system rejestrowania - łącznie z widokiem
-//        $form = $this->userManager->registerUser(
-//            $request->get('user')['plainPassword']['first'],
-//            $request->get('user')['userName'],
-//            $request->get('user')['email']
-//        );
-        //$form['form']->handleRequest($request);
-
-//        $user = new User();
-//        $form = $this->createForm(UserType::class, $user);
-//
-//        $form->handleRequest($request);
-//        if ($form->isSubmitted() && $form->isValid()) {
-//            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
-//            $user->setPassword($password);
-//
-//            $entityManager = $this->getDoctrine()->getManager();
-//            $entityManager->persist($user);
-//            $entityManager->flush();
-//
-//        };
-
-//        return $this->render(
-//            'registration/register.html.twig',
-//            array('form' => $form['form']->createView(),
-//                'errors' => $form['errors']
-//            )
-//        );
     }
 
     /**
