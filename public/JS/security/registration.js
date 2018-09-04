122b8f0d4ba8c1e69892e6f1b83145dab279e5fc
JarekDylewski <jaroslaw.dylewski@interia.pl>
2018-09-04 19:37:26 +0200

    Przeniesienie funkcjonalności do metod klasy

--- public/JS/security/registration.js
+++ public/JS/security/registration.js
@@ -4,16 +4,9 @@ $(document).ready(function () {
     let validation = new ValidationHints();
     let username = $('#userName');
     let usernameHints = $('#username_validation_hints');
-    let registrationButton = $('#register_button');
-
-    const emailReg = /^[a-z\d]+[\w\d.-]*@(?:[a-z\d]+[a-z\d-]+\.){1,5}[a-z]{2,6}$/i;
-    let emailRegExp = new RegExp(emailReg);
 
     $(username).focusin(function () {
-        $(usernameHints).empty();
-        $(usernameHints).removeClass('text-danger');
-        $(usernameHints).removeClass('text-success');
-        $(usernameHints).addClass('text-secondary');
+        validation.clearHint(usernameHints)
     });
 
     $(username).on('keypress', function () {
@@ -25,9 +18,7 @@ $(document).ready(function () {
         if (username.val().length < 3) {
             $(usernameHints).addClass('text-danger');
         } else {
-            $(usernameHints).removeClass('text-danger');
-            $(usernameHints).addClass('text-success');
-            $(usernameHints).html('OK!')
+            validation.everythingOkHint(usernameHints)
         }
 
         $.ajax({
@@ -40,8 +31,7 @@ $(document).ready(function () {
             }
         }).done(function (data) {
             if (data.username === true) {
-                $(usernameHints).html('Username already exists');
-                $(usernameHints).addClass('text-danger');
+                validation.somethingIsWrongHint(usernameHints, 'Username already exists!')
             }
         });
     });
@@ -49,10 +39,7 @@ $(document).ready(function () {
     let email = $('#email');
     let emailHints = $('#email_validation_hints');
     $(email).focusin(function () {
-        $(emailHints).empty();
-        $(emailHints).removeClass('text-danger');
-        $(emailHints).removeClass('text-success');
-        $(emailHints).addClass('text-secondary');
+        validation.clearHint(emailHints)
     });
 
     $(email).on('keypress', function () {
@@ -63,13 +50,10 @@ $(document).ready(function () {
         validation.minimumCharacterAlert(email, emailHints, 5);
         if (email.val().length < 5) {
             $(emailHints).addClass('text-danger');
-        } else if (!emailRegExp.test(email.val())) {
-            $(emailHints).html('Invalid e-mail!');
-            $(emailHints).addClass('text-danger');
+        } else if (validation.validateEmailHint(email.val()) === false) {
+            validation.somethingIsWrongHint(emailHints, 'Invalid e-mail!')
         } else {
-            $(emailHints).removeClass('text-danger');
-            $(emailHints).addClass('text-success');
-            $(emailHints).html('OK!')
+            validation.everythingOkHint(emailHints);
         }
         $.ajax({
             url: 'register/whetherUserExists',
@@ -81,8 +65,7 @@ $(document).ready(function () {
             }
         }).done(function (data) {
             if (data.email === true) {
-                $(emailHints).html('E-mail already exists');
-                $(emailHints).addClass('text-danger');
+                validation.somethingIsWrongHint(emailHints, 'E-mail already exists!');
             }
         });
     });
@@ -91,10 +74,7 @@ $(document).ready(function () {
     let passwordFirstHints = $('#password_first_validation_hints');
 
     $(passwordFirst).focusin(function () {
-        $(passwordFirstHints).empty();
-        $(passwordFirstHints).removeClass('text-danger');
-        $(passwordFirstHints).removeClass('text-success');
-        $(passwordFirstHints).addClass('text-secondary');
+        validation.clearHint(passwordFirst)
     });
 
     $(passwordFirst).on('keypress', function () {
@@ -106,9 +86,7 @@ $(document).ready(function () {
         if (passwordFirst.val().length < 5) {
             $(passwordFirstHints).addClass('text-danger');
         } else {
-            $(passwordFirstHints).removeClass('text-danger');
-            $(passwordFirstHints).addClass('text-success');
-            $(passwordFirstHints).html('OK!')
+            validation.everythingOkHint(passwordFirstHints);
         }
     });
 
@@ -116,10 +94,7 @@ $(document).ready(function () {
     let passwordSecondHints = $('#password_second_validation_hints');
 
     $(passwordSecond).focusin(function () {
-        $(passwordSecondHints).empty();
-        $(passwordSecondHints).removeClass('text-danger');
-        $(passwordSecondHints).removeClass('text-success');
-        $(passwordSecondHints).addClass('text-secondary');
+        validation.clearHint(passwordSecondHints)
     });
 
     $(passwordSecond).on('keypress', function () {
@@ -131,12 +106,9 @@ $(document).ready(function () {
         if (passwordSecond.val().length < 5) {
             $(passwordSecondHints).addClass('text-danger');
         } else if (passwordFirst.val() !== passwordSecond.val()) {
-            $(passwordSecondHints).addClass('text-danger');
-            $(passwordSecondHints).html('passwords not identical!')
+            validation.somethingIsWrongHint(passwordSecondHints, 'passwords not identical!')
         } else {
-            $(passwordSecondHints).removeClass('text-danger');
-            $(passwordSecondHints).addClass('text-success');
-            $(passwordSecondHints).html('OK!')
+            validation.everythingOkHint(passwordSecondHints)
         }
     });
 });
