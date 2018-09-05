9b2363853a285ff62abb69499774b50300bea1e3
JarekDylewski <jaroslaw.dylewski@interia.pl>
2018-09-05 22:40:37 +0200

    Usprawnienie licznika znaków - teraz działa dobrze przy każdej czynności kasowania znaków przez użytkownika.

--- public/JS/tutorials/ValidationHints.js
+++ public/JS/tutorials/ValidationHints.js
@@ -7,32 +7,37 @@ function ValidationHints() {
      * @param message {string} - message near number
      * @param messageForOne - message near number for 1 or 0
      */
-    this.charsLeft = function (classOrIDTextElement, classOrIDContainerForNumber, maxLength, message = ': characters left', messageForOne = ': character left') {
-//TODO dodać obliczanie pozostałych znaków po tym jak użytkownik zaznaczy sporą częśc tekstu i ją usunie
-        let strLength = $(classOrIDTextElement).val().length + 1;
-        if (strLength <= (maxLength - 2)) {
-            $(classOrIDContainerForNumber).html(maxLength - strLength + message);
-        } else if (strLength === (maxLength - 1)) {
-            $(classOrIDContainerForNumber).html(maxLength - strLength + messageForOne);
-        } else {
-            $(classOrIDContainerForNumber).html(strLength - strLength + messageForOne);
-        }
+    this.charsLeft = function (classOrIDTextElement, classOrIDContainerForNumber, maxLength, message = ': letters left', messageForOne = ': letter left') {
+        setTimeout(function () {
+            let strLength = $(classOrIDTextElement).val().length;
+            if (strLength <= (maxLength - 2)) {
+                $(classOrIDContainerForNumber).html(maxLength - strLength + message);
+            } else if (strLength === (maxLength - 1)) {
+                $(classOrIDContainerForNumber).html(maxLength - strLength + messageForOne);
+            } else {
+                $(classOrIDContainerForNumber).html(strLength - strLength + messageForOne);
+            }
+        }, 100);
+
         $(classOrIDTextElement).on('keydown', function (e) {
-            let strLength = $(classOrIDTextElement).val().length - 1;
-            let charsLeft = maxLength - strLength;
-            if (e.keyCode === 8) {
-                if (strLength === -1) {
-                    //do nothing
-                } else {
-                    if (strLength <= (maxLength - 2)) {
-                        $(classOrIDContainerForNumber).html(charsLeft++ + message);
-                    } else if (strLength === (maxLength - 1)) {
-                        $(classOrIDContainerForNumber).html(charsLeft++ + messageForOne);
+            setTimeout(function () {
+                let strLength = $(classOrIDTextElement).val().length;
+                let charsLeft = maxLength - strLength;
+                if (e.keyCode === 8) {
+                    if (strLength === -1) {
+                        //do nothing
                     } else {
-                        $(classOrIDContainerForNumber).html(charsLeft++ + messageForOne);
+                        if (strLength <= (maxLength - 2)) {
+                            $(classOrIDContainerForNumber).html(charsLeft++ + message);
+                        } else if (strLength === (maxLength - 1)) {
+                            $(classOrIDContainerForNumber).html(charsLeft++ + messageForOne);
+                        } else {
+                            $(classOrIDContainerForNumber).html(charsLeft++ + messageForOne);
+                        }
                     }
                 }
-            }
+            }, 100);
+
         });
     };
 
