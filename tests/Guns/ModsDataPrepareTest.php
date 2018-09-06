9fe8039823982de98afedb441a3c6614d898d556
JarekDylewski <jaroslaw.dylewski@interia.pl>
2018-09-06 18:00:58 +0200

    Uwzględnienie zmian które zaszły w klasach w testach

--- tests/Guns/ModsDataPrepareTest.php
+++ tests/Guns/ModsDataPrepareTest.php
@@ -2,43 +2,48 @@
 
 namespace App\Tests\Guns;
 
-
-use App\Data\PrepareData;
-use App\Guns\ModsDataPrepare;
+use App\Guns\FileGunRepository;
+use App\Guns\ModsDataProvider;
 use PHPUnit\Framework\TestCase;
 
 class ModsDataPrepareTest extends TestCase
 {
     private $PrepareData;
+
     protected function setUp()
     {
-        $this->PrepareData = new PrepareData(6);
+        $this->PrepareData = new FileGunRepository('src/Data/gunsData.ser');
     }
+
     public function testWhatReturnPrepareModID()
     {
-        $ModsDataPrepare = new ModsDataPrepare($this->PrepareData);
-        $modsIDArray = $ModsDataPrepare->prepareModID('ammo');
+
+        $modsDataPrepare = new ModsDataProvider($this->PrepareData->findById(6));
+        $modsIDArray = $modsDataPrepare->prepareModID('ammo');
         //testing whether the array contains 45 value
-        $this->assertContains(45,$modsIDArray);
+        $this->assertContains(45, $modsIDArray);
 
     }
+
     public function testWrongModificationTypeException()
     {
-        $ModsDataPrepare = new ModsDataPrepare($this->PrepareData);
+        $modsDataPrepare = new ModsDataProvider($this->PrepareData->findById(6));
         $this->expectException('\\Exception');
-        $ModsDataPrepare->prepareModID('screw');
+        $modsDataPrepare->prepareModID('screw');
         $this->fail('array does not have screwID key');
     }
+
     public function testWhatReturnPrepareModData()
     {
-        $ModsDataPrepare = new ModsDataPrepare($this->PrepareData);
-        $modsID = $ModsDataPrepare->prepareModID('ammo');
-        $modsData = $ModsDataPrepare->prepareModData($modsID,'getAmmoData');
+        $modsDataPrepare = new ModsDataProvider($this->PrepareData->findById(6));
+        $modsID = $modsDataPrepare->prepareModID('ammo');
+        $modsData = $modsDataPrepare->prepareModData($modsID, 'src/Data/ammoData.ser');
         $modName = $modsData[0];
         // Testing does the array contain 'name' key
-        $this->assertArrayHasKey('name',$modName);
+        $this->assertArrayHasKey('name', $modName);
 
     }
+
     protected function tearDown()
     {
         unset($this->PrepareData);
