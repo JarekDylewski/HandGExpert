f0ab7eaedad277c7953a9df163c8d55da290de26
JarekDylewski <jaroslaw.dylewski@interia.pl>
2018-09-07 21:32:45 +0200

    Dodanie zapisywania wyjątków w logach

--- src/Guns/GunManager.php
+++ src/Guns/GunManager.php
@@ -14,17 +14,33 @@ class GunManager
         $this->gunRepository = new FileGunRepository('../src/Data/gunsData.ser');
     }
 
+    /**
+     * @param Int $gunID
+     * @return array
+     * @throws GunsNotFoundException
+     * @throws \Exception
+     */
     public function getAllDataForView(Int $gunID): array
     {
         try {
             $weapon = $this->gunRepository->findById($gunID);
         } catch (GunsNotFoundException $e) {
-            $test = 'test';
-            dump($test);
+            file_put_contents('../var/log/exceptions.log',
+                '[' . date('Y-m-d H:i:s') . '] ' . sprintf('| %s | line: %s | file: %s | code: %s', $e->getMessage(),
+                    $e->getLine(), $e->getFile(), $e->getCode()) . "\n", FILE_APPEND);
+            throw $e;
         }
 
         $modsDataPrepare = new ModsDataProvider($weapon);
-        $ammoModsArray = $modsDataPrepare->prepareModID('ammo');
+        try {
+            $ammoModsArray = $modsDataPrepare->prepareModID('ammo');
+        } catch (\Exception $e) {
+            file_put_contents('../var/log/exceptions.log',
+                '[' . date('Y-m-d H:i:s') . '] ' . sprintf('| %s | line: %s | file: %s | code: %s', $e->getMessage(),
+                    $e->getLine(), $e->getFile(), $e->getCode()) . "\n", FILE_APPEND);
+            throw $e;
+        }
+
         $ammoData = $modsDataPrepare->prepareModData($ammoModsArray, '../src/Data/ammoData.ser');
 
         return [
