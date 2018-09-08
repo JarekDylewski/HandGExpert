3906a07dda013240aad8e13bf1659146185892af
JarekDylewski <jaroslaw.dylewski@interia.pl>
2018-09-08 22:14:54 +0200

    Obsługa wiadomości o powodzeniu dla edycji artykułu

--- public/JS/tutorials/tutorialArticleAddedNotAdded.js
+++ public/JS/tutorials/tutorialArticleAddedNotAdded.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     let message = $('#tutorial_message').html();
-    if (message.search(/Added/) > 0) {
+    if (message.search(/Added/) > 0 || message.search(/Success/)) {
         $('#tutorial_message').addClass('alert-success h3');
     } else {
         $('#tutorial_message').addClass('alert-danger h6');
