daa8dc1b312ac19c4fa6399c7f968a144bba2f43
JarekDylewski <jaroslaw.dylewski@interia.pl>
2018-09-13 23:44:06 +0200

    Dodanie kolejnych szablonow oraz modyfikacja poprzednich - głównie dodanie bootstrapColSize jako argument wejsciowy dla metody, w celu okreslenia jaka szerokosc bedzie mial dany szalon

--- public/JS/compare/CompareTemplates.js
+++ public/JS/compare/CompareTemplates.js
@@ -1,10 +1,6 @@
 function CompareTemplates() {
     this.getCompare1 = function (classOrID, weaponId = 1) {
-        let padding = 'pl-0';
-        if (weaponId % 2 === 0) {
-            padding = 'pl-1';
-        }
-        $(classOrID).append(`<div class="col-6 p-0 m-0 ${padding}">
+        $(classOrID).append(`<div class="col-6 p-0 m-0">
         <div id="compareWeaponName${weaponId}" class="row w-100 m-0 border-bottom">
             Miejsce na nazwe broni
         </div>
@@ -28,16 +24,12 @@ function CompareTemplates() {
     </div>`);
     };
 
-    this.getCompare2 = function (classOrID, weaponId = 1) {
-        let padding = 'pl-0';
-        if (weaponId % 2 === 0) {
-            padding = 'pl-1';
-        }
-        $(classOrID).append(`<div class="col-6 p-0 m-0 compareWeaponDescriptionContainer${weaponId} ${padding}">
+    this.getCompare2 = function (classOrID, weaponIndexInStorage = 1, boodstrapColSize = 2) {
+        $(classOrID).append(`<div class="col-${boodstrapColSize} p-0 m-0 compareWeaponDescriptionContainer${weaponIndexInStorage}">
         <div class="row w-100 m-0 border-bottom text-right">
-            <img style="opacity: 0.6;" class="position-absolute mb-3" width="150" height="80"
-                 src='/img/gun_ribbons/pps-43.png'>
-            <span id="compareWeaponName${weaponId}" style="z-index: 2;" class="w-100 text-right font-size-14 mt-4 pt-4">miejsce na nazwe broni</span>
+            <img id="compareWeaponImg${weaponIndexInStorage}" style="opacity: 0.6;" class="position-absolute mb-3" width="150" height="80"
+                 src='/img/gunRibbonsByID/0.png'>
+            <span id="compareWeaponName${weaponIndexInStorage}" style="z-index: 2;" class="w-100 text-right font-size-14 mt-4 pt-4">miejsce na nazwe broni</span>
         </div>
         <div class="row w-100 m-0 border-bottom">
             <div class="col-2 ml-0 mr-auto p-0 border-right"><img width="30px" height="30px"
@@ -45,7 +37,7 @@ function CompareTemplates() {
                                                                   alt="ammo"
                                                                   data-placement="top"
                 ></div>
-            <div id="compareAmmoMod${weaponId}" class="col-10 text-left pl-1">Nazwa moda</div>
+            <div id="compareAmmoMod${weaponIndexInStorage}" class="col-10 text-left pl-1">Nazwa moda</div>
         </div>
         <div class="row w-100 m-0 border-bottom">
             <div class="col-2 ml-0 mr-auto p-0 border-right"><img width="30px" height="30px"
@@ -53,81 +45,93 @@ function CompareTemplates() {
                                                                   alt="crosshair"
                                                                   data-placement="top"
                 ></div>
-            <div id="compareCrosshairMod${weaponId}" class="col-10 text-left pl-1">Nazwa moda</div>
+            <div id="compareCrosshairMod${weaponIndexInStorage}" class="col-10 text-left pl-1">Nazwa moda</div>
         </div>
         <div class="row w-100 m-0 border-bottom">
             <div class="col-2 ml-0 mr-auto p-0 border-right"><img width="30px" height="30px"
                                                                   src='/img/mody/trigger.png'
                                                                   alt="trigger"></div>
-            <div id="compareTriggerMod${weaponId}" class="col-10 text-left pl-1">Nazwa moda</div>
+            <div id="compareTriggerMod${weaponIndexInStorage}" class="col-10 text-left pl-1">Nazwa moda</div>
         </div>
         <div class="row w-100 m-0 border-bottom">
             <div class="col-2 ml-0 mr-auto p-0 border-right"><img width="30px" height="30px"
                                                                   src='/img/mody/spring.png'
                                                                   alt="spring"
                 ></div>
-            <div id="compareSpringMod${weaponId}" class="col-10 text-left pl-1">Nazwa moda</div>
+            <div id="compareSpringMod${weaponIndexInStorage}" class="col-10 text-left pl-1">Nazwa moda</div>
         </div>
         <div class="row w-100 m-0 border-bottom">
             <div class="col-2 ml-0 mr-auto p-0 border-right"><img width="30px" height="30px"
                                                                   src='/img/mody/barrel.png'
                                                                   alt="barrel"
                 ></div>
-            <div id="compareBarrelMod${weaponId}" class="col-10 text-left pl-1">Nazwa moda</div>
+            <div id="compareBarrelMod${weaponIndexInStorage}" class="col-10 text-left pl-1">Nazwa moda</div>
         </div>
     </div>`)
     };
-
-    this.getCompareStatisticsForTwoWeapons = function (classOrID) {
-
+    this.getCompareStatistics = function (classOrID, weaponIndexInStorage, boodstrapColSize = 2) {
         $(classOrID).append(`
-                <div class="row w-100 pt-3">
-                    <div class="col-4 p-0 border-bottom">Damage:</div><div id="damage1" class="col-2 border-right border-bottom p-0 text-center">test</div><div id="damage2" class="col-2 border-left border-bottom  p-0 text-center">test</div>
+            <div class="col-${boodstrapColSize} border-right border-left statisticsFor${weaponIndexInStorage}">
+                <div class="row">
+                    <div id="damage${weaponIndexInStorage}" class="col-12 border-bottom p-0 text-center"> - </div>
+           
+                    <div id="muzzleVelocity${weaponIndexInStorage}" class="col-12 border-bottom  p-0 text-center"> - </div>
+               
+                    <div id="rateOfFire${weaponIndexInStorage}" class="col-12 border-bottom  p-0 text-center"> - </div>
+               
+                    <div id="range${weaponIndexInStorage}" class="col-12 border-bottom  p-0 text-center"> - </div>
+               
+                    <div id="recoil${weaponIndexInStorage}" class="col-12 border-bottom  p-0 text-center"> - </div>
+               
+                    <div id="timeToKill${weaponIndexInStorage}" class="col-12 border-bottom  p-0 text-center"> - </div>
+    
+                    <div id="costs${weaponIndexInStorage}" class="col-12 border-bottom  p-0 text-center"> - </div>
+               
+                    <div id="spawnDelay${weaponIndexInStorage}" class="col-12 border-bottom  p-0 text-center"> - </div>
+               
+                    <div id="reloadTime${weaponIndexInStorage}" class="col-12 border-bottom  p-0 text-center"> - </div>
+               
+                    <div id="equipmentPoint${weaponIndexInStorage}" class="col-12 border-bottom  p-0 text-center"> - </div>
+               
+                    <div id="ammoCapacity${weaponIndexInStorage}" class="col-12 border-bottom  p-0 text-center"> - </div>
+               
+                    <div id="magazines${weaponIndexInStorage}" class="col-12 border-bottom  p-0 text-center"> - </div>
+               
+                    <div id="equipTime${weaponIndexInStorage}" class="col-12 border-bottom  p-0 text-center"> - </div>
+               
+                    <div id="additionalFatigue${weaponIndexInStorage}" class="col-12 border-bottom  p-0 text-center"> - </div>
+               
+                    <div id="aimingTime${weaponIndexInStorage}" class="col-12 border-bottom  p-0 text-center"> - </div>
+               
+                    <div id="useWhileRunning${weaponIndexInStorage}" class="col-12  p-0 text-center"> - </div>
                 </div>
-                <div class="row w-100">
-                    <div class="col-4 p-0">Muzzle velocity:</div><div id="muzzleVelocity1" class="col-2 border-right border-bottom  p-0 text-center">test</div><div id="muzzleVelocity2" class="col-2 border-left border-bottom  p-0 text-center">test</div>
+            </div>
+        `);
+    };
+
+    this.getStatisticsCompareLegend = function (classOrID, boodstrapColSize = 4) {
+        $(classOrID).append(`
+            <div class="col-${boodstrapColSize}">
+                <div class="row">
+                    <div class="col-12 p-0 border-bottom">Damage:</div>
+                    <div class="col-12 p-0 border-bottom">Muzzle velocity:</div>
+                    <div class="col-12 p-0 border-bottom">Rate of fire:</div>
+                    <div class="col-12 p-0 border-bottom">Range:</div>
+                    <div class="col-12 p-0 border-bottom">Recoil:</div>
+                    <div class="col-12 p-0 border-bottom">Time to kill:</div>
+                    <div class="col-12 p-0 border-bottom">Costs:</div>
+                    <div class="col-12 p-0 border-bottom">Spawn delay:</div>
+                    <div class="col-12 p-0 border-bottom">Reload time:</div>
+                    <div class="col-12 p-0 border-bottom">Equipment points:</div>
+                    <div class="col-12 p-0 border-bottom">Ammo capacity:</div>
+                    <div class="col-12 p-0 border-bottom">Magazines:</div>
+                    <div class="col-12 p-0 border-bottom">Equip time:</div>
+                    <div class="col-12 p-0 border-bottom">Additional fatigue:</div>
+                    <div class="col-12 p-0 border-bottom">Aiming time:</div>
+                    <div class="col-12 p-0">Use while running:</div>
                 </div>
-                <div class="row w-100">
-                    <div class="col-4 p-0 border-bottom">Rate of fire:</div><div id="rateOfFire1" class="col-2 border-right border-bottom  p-0 text-center">test</div><div id="rateOfFire2" class="col-2 border-left border-bottom  p-0 text-center">test</div>
-                </div>
-                <div class="row w-100">
-                    <div class="col-4 p-0">Range:</div><div id="range1" class="col-2 border-right border-bottom  p-0 text-center">test</div><div id="range2" class="col-2 border-left border-bottom  p-0 text-center">test</div>
-                </div>
-                <div class="row w-100">
-                    <div class="col-4 p-0 border-bottom">Recoil:</div><div id="recoil1" class="col-2 border-right border-bottom  p-0 text-center">test</div><div id="recoil2" class="col-2 border-left border-bottom  p-0 text-center">test</div>
-                </div>
-                <div class="row w-100">
-                    <div class="col-4 p-0">Time to kill:</div><div id="timeToKill1" class="col-2 border-right border-bottom  p-0 text-center">test</div><div id="timeToKill2" class="col-2 border-left border-bottom  p-0 text-center">test</div>
-                </div>
-                <div class="row w-100">
-                    <div class="col-4 p-0 border-bottom">Costs:</div><div id="costs1" class="col-2 border-right border-bottom  p-0 text-center">test</div><div id="costs2" class="col-2 border-left border-bottom  p-0 text-center">test</div>
-                </div>
-                <div class="row w-100">
-                    <div class="col-4 p-0">Spawn delay:</div><div id="spawnDelay1" class="col-2 border-right border-bottom  p-0 text-center">test</div><div id="spawnDelay2" class="col-2 border-left border-bottom  p-0 text-center">test</div>
-                </div>
-                <div class="row w-100">
-                    <div class="col-4 p-0 border-bottom">Reload time:</div><div id="reloadTime1" class="col-2 border-right border-bottom  p-0 text-center">test</div><div id="reloadTime2" class="col-2 border-left border-bottom  p-0 text-center">test</div>
-                </div>
-                <div class="row w-100">
-                    <div class="col-4 p-0">Equipment points:</div><div id="equipmentPoint1" class="col-2 border-right border-bottom  p-0 text-center">test</div><div id="equipmentPoint2" class="col-2 border-left border-bottom  p-0 text-center">test</div>
-                </div>
-                <div class="row w-100">
-                    <div class="col-4 p-0 border-bottom">Ammo capacity:</div><div id="ammoCapacity1" class="col-2 border-right border-bottom  p-0 text-center">test</div><div id="ammoCapacity2" class="col-2 border-left border-bottom  p-0 text-center">test</div>
-                </div>
-                <div class="row w-100">
-                    <div class="col-4 p-0">Magazines:</div><div id="magazines1" class="col-2 border-right border-bottom  p-0 text-center">test</div><div id="magazines2" class="col-2 border-left border-bottom  p-0 text-center">test</div>
-                </div>
-                <div class="row w-100">
-                    <div class="col-4 p-0 border-bottom">Equip time:</div><div id="equipTime1" class="col-2 border-right border-bottom  p-0 text-center">test</div><div id="equipTime2" class="col-2 border-left border-bottom  p-0 text-center">test</div>
-                </div>
-                <div class="row w-100">
-                    <div class="col-4 p-0">Additional fatigue:</div><div id="additionalFatigue1" class="col-2 border-right border-bottom  p-0 text-center">test</div><div id="additionalFatigue2" class="col-2 border-left border-bottom  p-0 text-center">test</div>
-                </div>
-                <div class="row w-100">
-                    <div class="col-4 p-0 border-bottom">Aiming time:</div><div id="aimingTime1" class="col-2 border-right border-bottom  p-0 text-center">test</div><div id="aimingTime2" class="col-2 border-left border-bottom  p-0 text-center">test</div>
-                </div>
-                <div class="row w-100">
-                    <div class="col-4 p-0">Use while running:</div><div id="useWhileRunning1" class="col-2 border-right  p-0 text-center">test</div><div id="useWhileRunning2" class="col-2 border-left  p-0 text-center">test</div>
-                </div>`);
-    }
+            </div>
+        `);
+    };
+
 }
