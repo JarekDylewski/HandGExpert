357e4cd062c2c9459500b891e4fa3b9209cfe8aa
JarekDylewski <jaroslaw.dylewski@interia.pl>
2018-09-13 23:48:40 +0200

    Obsługa bocznego panelu oraz zapisywania broni do localStorage - początkowa wersja

--- public/JS/compare/compare.js
+++ public/JS/compare/compare.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
+
     function insertData(Id, weapons) {
         $('#damage' + Id).html(weapons[Id].dmg);
         $('#muzzleVelocity' + Id).html(weapons[Id].muzzleVelocit);
         $('#rateOfFire' + Id).html(weapons[Id].rateOfFire);
         $('#range' + Id).html(weapons[Id].rangeNear);
-        $('#recoil1').html(weapons[Id].recoil);
+        $('#recoil' + Id).html(weapons[Id].recoil);
         $('#timeToKill' + Id).html(weapons[Id].timeToKill);
         $('#costs' + Id).html(weapons[Id].costs);
         $('#spawnDelay' + Id).html(weapons[Id].spawnDelay);
@@ -17,6 +18,14 @@ $(document).ready(function () {
         $('#aimingTime' + Id).html(weapons[Id].aimingTime);
         $('#useWhileRunning' + Id).html(weapons[Id].useWhileRunning);
         $('#compareWeaponName' + Id).html(weapons[Id].name);
+        $('#compareAmmoMod' + Id).html(weapons[Id].ammo);
+        $('#compareCrosshairMod' + Id).html(weapons[Id].crosshair);
+        $('#compareTriggerMod' + Id).html(weapons[Id].trigger);
+        $('#compareSpringMod' + Id).html(weapons[Id].spring);
+        $('#compareBarrelMod' + Id).html(weapons[Id].barrel);
+        if ($('#compareWeaponImg' + Id)) {
+            $('#compareWeaponImg' + Id).attr('src', '/img/gunRibbonsByID/' + weapons[Id].gunId + '.png')
+        }
     }
 
     let compareTemplates = new CompareTemplates();
@@ -24,51 +33,85 @@ $(document).ready(function () {
         let weapons = JSON.parse(localStorage.getItem('weapons'));
         let storageLengthVar = weapons.length;
         if (storageLengthVar <= 3) {
-            compareTemplates.getCompareStatisticsForTwoWeapons('.statisticsForTwoWeapons');
-            compareTemplates.getCompare2('.compareSpace', 1);
-            compareTemplates.getCompare2('.compareSpace', 2);
-            $('.compareWeaponDescriptionContainer2').addClass('pl-1');
+            compareTemplates.getCompare2('.compareSpace', 1, 6);
+            compareTemplates.getCompare2('.compareSpace', 2, 6);
+            compareTemplates.getStatisticsCompareLegend('.statisticsForTwoWeapons', 4);
+            compareTemplates.getCompareStatistics('.statisticsForTwoWeapons', 1, 2);
+
+
+            //$('.compareWeaponDescriptionContainer2').addClass('pl-1');
             insertData(1, weapons);
             if (storageLengthVar > 2) {
+
+                compareTemplates.getCompareStatistics('.statisticsForTwoWeapons', 2, 2);
                 insertData(2, weapons);
             }
         } else {
             let i = 1;
-            while (i <= storageLengthVar - 1) {
-                compareTemplates.getCompare2('.compareSpace', i);
+            while (i < storageLengthVar) {
+                compareTemplates.getCompare2('.compareSpace', i, 6);
+                insertData(i, weapons);
                 i++;
             }
+            if ($('.compareWeaponDescriptionContainer11')) {
+                $('.compareWeaponDescriptionContainer11').empty()
+            }
         }
+        let compareStatistics = new CompareStatistics();
+        compareStatistics.compareTwoStatistics($('#damage1'), $('#damage2'), 'more');
+        compareStatistics.compareTwoStatistics($('#muzzleVelocity1'), $('#muzzleVelocity2'), 'more');
+        compareStatistics.compareTwoStatistics($('#rateOfFire1'), $('#rateOfFire2'), 'more');
+        compareStatistics.compareTwoStatistics($('#range1'), $('#range2'), 'more');
+        compareStatistics.compareTwoStatistics($('#recoil1'), $('#recoil2'), 'less');
+        compareStatistics.compareTwoStatistics($('#timeToKill1'), $('#timeToKill2'), 'less');
+        compareStatistics.compareTwoStatistics($('#costs1'), $('#costs2'), 'less');
+        compareStatistics.compareTwoStatistics($('#spawnDelay1'), $('#spawnDelay2'), 'less');
+        compareStatistics.compareTwoStatistics($('#reloadTime1'), $('#reloadTime2'), 'less');
+        compareStatistics.compareTwoStatistics($('#equipmentPoint1'), $('#equipmentPoint2'), 'less');
+        compareStatistics.compareTwoStatistics($('#ammoCapacity1'), $('#ammoCapacity2'));
+        compareStatistics.compareTwoStatistics($('#equipTime1'), $('#equipTime2'), 'less');
+        compareStatistics.compareTwoStatistics($('#magazines1'), $('#magazines2'));
+        compareStatistics.compareTwoStatistics($('#additionalFatigue1'), $('#additionalFatigue2'), 'less');
+        compareStatistics.compareTwoStatistics($('#aimingTime1'), $('#aimingTime2'), 'less');
+        compareStatistics.compareTwoStatistics($('#useWhileRunning1'), $('#useWhileRunning2'), 'more');
     }
 
     $('#clear_compare').on('click', function () {
         localStorage.clear();
         $('.compareSpace').empty();
+        $('.statisticsForTwoWeapons').empty();
     });
 
     let iter = 0;
     let weapons = [];
     $('#compare_weapon').on('click', function () {
-        let objectCopy = Object.assign({}, statistics);
+        let weaponStatisticsCopy = Object.assign({}, statistics);
+        let modsAndGunIdCopy = Object.assign({}, modsAndGunSelectedByUser);
         let weaponData = {
-            "dmg": objectCopy.getDamage().toFixed(3),
-            "dmgFar": objectCopy.getDamageFar().toFixed(3),
-            "muzzleVelocit": Math.round(objectCopy.getMuzzleVelocity()),
-            "rateOfFire": objectCopy.getRateOfFire().toFixed(1),
-            "rangeNear": objectCopy.getRangeNear().toFixed(1),
-            "recoil": objectCopy.getRecoilModifier().toFixed(2),
-            "timeToKill": objectCopy.getTimeToKill().toFixed(1),
-            "costs": objectCopy.getPricePerShot().toFixed(2),
-            "spawnDelay": objectCopy.getSpawnDelay().toFixed(1),
-            "reloadTime": objectCopy.getReloadTime().toFixed(1),
-            "equipmentPoint": objectCopy.getEquipmentPoints(),
-            "ammoCapacity": objectCopy.getAmmoCapacity(),
-            "magazines": objectCopy.getMagazines(),
-            "equipTime": objectCopy.getEquipTime(),
-            "additionalFatigue": objectCopy.getAdditionalFatuque(),
-            "aimingTime": Math.round(objectCopy.getAimingTime()),
-            "useWhileRunning": objectCopy.getUseWhileRunning(),
-            "name": $('#weaponName').html()
+            "gunId": modsAndGunIdCopy.getGunId(),
+            "dmg": weaponStatisticsCopy.getDamage().toFixed(3),
+            "dmgFar": weaponStatisticsCopy.getDamageFar().toFixed(3),
+            "muzzleVelocit": Math.round(weaponStatisticsCopy.getMuzzleVelocity()),
+            "rateOfFire": weaponStatisticsCopy.getRateOfFire().toFixed(1),
+            "rangeNear": weaponStatisticsCopy.getRangeNear().toFixed(1),
+            "recoil": weaponStatisticsCopy.getRecoilModifier().toFixed(2),
+            "timeToKill": weaponStatisticsCopy.getTimeToKill().toFixed(1),
+            "costs": weaponStatisticsCopy.getPricePerShot().toFixed(2),
+            "spawnDelay": weaponStatisticsCopy.getSpawnDelay().toFixed(1),
+            "reloadTime": weaponStatisticsCopy.getReloadTime().toFixed(1),
+            "equipmentPoint": weaponStatisticsCopy.getEquipmentPoints(),
+            "ammoCapacity": weaponStatisticsCopy.getAmmoCapacity(),
+            "magazines": weaponStatisticsCopy.getMagazines(),
+            "equipTime": weaponStatisticsCopy.getEquipTime(),
+            "additionalFatigue": weaponStatisticsCopy.getAdditionalFatuque(),
+            "aimingTime": Math.round(weaponStatisticsCopy.getAimingTime()),
+            "useWhileRunning": weaponStatisticsCopy.getUseWhileRunning(),
+            "name": $('#weaponName').html(),
+            "ammo": $('.js-mod-name-list-ammo').html(),
+            "crosshair": $('.js-mod-name-list-crosshair').html(),
+            "trigger": $('.js-mod-name-list-trigger').html(),
+            "spring": $('.js-mod-name-list-spring').html(),
+            "barrel": $('.js-mod-name-list-barrel').html(),
         };
 
         if (localStorage.getItem('weapons')) {
@@ -94,19 +137,22 @@ $(document).ready(function () {
 
             let compareName = $('#compareWeaponName1');
             if (compareName.length < 1) {
-                compareTemplates.getCompareStatisticsForTwoWeapons('.statisticsForTwoWeapons');
-                compareTemplates.getCompare2('.compareSpace', 1);
-                compareTemplates.getCompare2('.compareSpace', 2);
+                compareTemplates.getStatisticsCompareLegend('.statisticsForTwoWeapons', 4);
+                compareTemplates.getCompareStatistics('.statisticsForTwoWeapons', 1, 2);
+                compareTemplates.getCompare2('.compareSpace', 1, 6);
+                compareTemplates.getCompare2('.compareSpace', 2, 6);
                 $('.compareWeaponDescriptionContainer2').addClass('pl-1');
             }
             insertData(1, weapons);
             if (length === 3) {
+                compareTemplates.getCompareStatistics('.statisticsForTwoWeapons', 2, 2);
                 insertData(2, weapons);
 
             } else if (length > 3 && iter <= 10) {
                 console.log(length);
                 $('.statisticsForTwoWeapons').empty();
-                compareTemplates.getCompare2('.compareSpace', iter);
+                compareTemplates.getCompare2('.compareSpace', iter, 6);
+                insertData(iter, weapons);
             }
         }
     });
