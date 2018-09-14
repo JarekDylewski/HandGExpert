45de8201405d77f7c65c7310ad69f9ee811df20d
JarekDylewski <jaroslaw.dylewski@interia.pl>
2018-09-14 23:05:13 +0200

    Dodanie logiki porównywania dowolnej liczby statystyk, metoda jednoczesnie odpowiednio koloruje dane miejsce.

--- public/JS/compare/CompareStatistics.js
+++ public/JS/compare/CompareStatistics.js
@@ -1,5 +1,56 @@
-function CompareStatistics() {
-    this.compareTwoStatistics = function (HTMLstat1, HTMLstat2, moreOrLess = 'more') {
+class CompareStatistics {
+
+    /**
+     * @param weapons[] - array with weapon objects
+     * @param key - key in weapon object example: {"key1":50, "key2":40, "foo":10,"bar":20}
+     * @param changePlaceColor - class or Id where the color will be changed
+     * @param colors[] - array with colors
+     * @param moreOrLess - determines, "more" - is better or "less" - is better
+     */
+    compareAllStatistics(weapons, key, changePlaceColor, colors, moreOrLess = 'more') {
+
+        let length = weapons.length;
+        let weaponValues = [];
+        for (let i = 1; i < length; i++) {
+            weaponValues[i] = parseFloat(weapons[i][key])
+        }
+
+        //If the user sets too few colors, the colors will be randomly set
+        let colorsLength = colors.length;
+        if (colorsLength < length) {
+            let random = new Random();
+            for (let i = colorsLength; i < length; i++) {
+                colors[i] = random.getRandomColor();
+            }
+        }
+
+
+        let valueOrderDesc = weaponValues.sort(function (a, b) {
+            return b - a
+        });
+
+        function uniqueArray(value, index, self) {
+            return self.indexOf(value) === index;
+        }
+
+        valueOrderDesc = valueOrderDesc.filter(uniqueArray);
+        if (moreOrLess == 'less') {
+            valueOrderDesc.reverse();
+        }
+
+        weapons.forEach(function (value, index) {
+            if (index > 0) {
+                valueOrderDesc.forEach(function (ordValue, ordIndex) {
+                    if (parseFloat(value[key]) === parseFloat(ordValue)) {
+                        $(changePlaceColor + index).css('color', colors[ordIndex]);
+                    }
+                })
+
+            }
+        });
+    }
+
+    compareTwoStatistics(HTMLstat1, HTMLstat2, moreOrLess = 'more') {
         let HTMLstat1ValParsed = parseFloat(HTMLstat1.html());
         let HTMLstat2ValParsed = parseFloat(HTMLstat2.html());
         if (moreOrLess == 'more') {
