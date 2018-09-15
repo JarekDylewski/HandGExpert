4fbf7606e6ac2e86fd3f59c201ff7c039e7f159d
JarekDylewski <jaroslaw.dylewski@interia.pl>
2018-09-15 23:29:40 +0200

    Dodanie obsługi usuwania broni z porównywarki.

--- public/JS/compare/compareAllWeapons.js
+++ public/JS/compare/compareAllWeapons.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
             dataForTemplates.insertData(i, weapons);
             i++;
         }
-        if (i > 5) {
+        if (i >= 5) {
             compareTemplates.getStatisticsCompareLegend('.compareLegend2', 12);
         }
         compareTemplates.getStatisticsCompareLegend('.compareLegend1', 12);
@@ -71,11 +71,84 @@ $(document).ready(function () {
 
     let compareDeleteItem = $('.compareDeleteItem');
     compareDeleteItem.on('mouseover', function () {
-        let itemId = $(this).attr('id').substr(17, 1);
+        let itemId = $(this).attr('id').substr(17, 2);
         $('.compareWeaponDescriptionContainer' + itemId + ',.statisticsFor' + itemId).css('background-color', 'rgba(255,103,93,0.05)');
     });
+
     compareDeleteItem.on('mouseout', function () {
-        let itemId = $(this).attr('id').substr(17, 1);
+        let itemId = $(this).attr('id').substr(17, 2);
         $('.compareWeaponDescriptionContainer' + itemId + ',.statisticsFor' + itemId).css('background-color', '');
-    })
+    });
+
+    compareDeleteItem.on('click', function (e) {
+        let allContainers = $('div[class *= "compareWeaponDescriptionContainer"]');
+        let targetContainer = e.target.parentElement.parentElement.parentElement;
+        let itemId = $(this).attr('id').substr(17, 2);
+        let i;
+        for (i = 0; i < allContainers.length; i++) {
+            if (targetContainer.getAttribute('class') === allContainers[i].getAttribute('class')) {
+                $('.compareWeaponDescriptionContainer' + itemId + ',.statisticsFor' + itemId).animate({
+                    opacity: 0.1,
+                    bottom: "15rem",
+                }, 300, function () {
+                    this.remove()
+                });
+
+                if (i < 5) {
+                    //TODO dokonczyc przenoszenie elementu
+                    let elemToCut = $('.compareWeaponDesc2 div[class *= "compareWeaponDescriptionContainer"]:first-child');
+                    console.log(elemId.getCla);
+                    let statisticsElemToCut = $('.statisticsFor' + elemId);
+                    elemToCut.remove();
+                    statisticsElemToCut.remove();
+
+                    console.log(' ten element trzeba przeniesc' + elemToCut)
+                }
+
+
+                let weaponsInStorage = localStorage.getItem('weapons');
+                let weapons = JSON.parse(weaponsInStorage);
+                weapons[i + 1] = undefined;
+
+                let weaponsToSave = [];
+                weaponsToSave[0] = undefined;
+                for (let i = 1; i < weapons.length; i++) {
+                    if (weapons[i] !== undefined) {
+                        weaponsToSave.push(weapons[i])
+                    }
+                }
+
+                localStorage.clear();
+                localStorage.setItem('weapons', JSON.stringify(weaponsToSave));
+
+                if (allContainers.length < 7) {
+                    $('.compareLegend2').empty()
+                }
+
+                if (weaponsToSave.length <= 1) {
+                    localStorage.clear();
+                    $('.compareLegend1').empty()
+                }
+            }
+        }
+
+        // if(targetContainer.getAttribute('class') === $("div[class *= 'compareWeaponDescriptionContainer']:eq(0)").attr('class')){
+        //     console.log('===')
+        // }
+        // if ($('.compareWeaponDescriptionContainer1').attr('id') == $('.compareWeaponDescriptionContainer1').attr('id')){
+        //     console.log('test');
+        // }
+        //console.log($("div[class *= 'compareWeaponDescriptionContainer']:eq(0)"));
+
+        // let itemId = $(this).attr('id').substr(17, 2);
+        // $('.compareWeaponDescriptionContainer' + itemId + ',.statisticsFor' + itemId).animate({
+        //     opacity: 0.1,
+        //     bottom:"15rem",
+        // },300, function () {
+        //     this.remove()
+        // });
+
+
+        //location.reload();
+    });
 });
