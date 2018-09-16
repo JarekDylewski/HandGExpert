e201fc9dab3aa7fcefcae7ebf9466ec9f62a93c0
JarekDylewski <jaroslaw.dylewski@interia.pl>
2018-09-16 23:53:02 +0200

    Skrypt wyswietla informację, że nic nie ma do pokazania w orzypadku gdy localStorage jest puste, dodanie przycisku powrotu do glownego panelu, Dodanie większej ilości kolorów sugerujących kolejność od najgorszej do najlepszej statystyki

--- public/JS/compare/compareAllWeapons.js
+++ public/JS/compare/compareAllWeapons.js
@@ -1,19 +1,38 @@
 $(document).ready(function () {
+    if (!localStorage.getItem('weapons')) {
+        $('.compareWeaponDesc1').append(`
+            <div class="col-6 text-center alert alert-info h1 offset-2 mt-5 border-info">Nothing to show</div>
+        `)
+    }
+    $('#back').on('click', function () {
+        window.history.back();
+    });
     var dataForTemplates = new DataProviderForCompareTemplate();
     if (localStorage.getItem('weapons')) {
 
+        // var colors = [
+        //     '#cd4a22',
+        //     '#db6831',
+        //     '#e28743',
+        //     '#dfb562',
+        //     '#e0d681',
+        //     '#feffd9',
+        //     '#aac599',
+        //     '#90bc85',
+        //     '#73b43e',
+        //     '#00873d'
+        // ];
         var colors = [
-            // '',
-            '#cd4a22',
+            '#643019',
             '#db6831',
             '#e28743',
             '#dfb562',
-            '#e0d681',
-            '#feffd9',
-            '#aac599',
-            '#90bc85',
-            '#73b43e',
-            '#00873d'
+            '#194900',
+            '#288a00',
+            '#3dff00',
+            '#070063',
+            '#000dff',
+            '#00b8f9'
         ];
         colors.reverse();
 
@@ -25,7 +44,7 @@ $(document).ready(function () {
         while (i < storageLengthVar) {
             if (i <= 5) {
                 compareTemplates.getCompare2('.compareWeaponDesc1', i, 2);
-                compareTemplates.getCompareStatistics('.compareStatistics1', i, 2)
+                compareTemplates.getCompareStatistics('.compareStatistics1', i, 2);
 
 
             } else {
@@ -36,14 +55,11 @@ $(document).ready(function () {
             dataForTemplates.insertData(i, weapons);
             i++;
         }
-        if (i >= 5) {
+        if (i >= 7) {
             compareTemplates.getStatisticsCompareLegend('.compareLegend2', 12);
         }
         compareTemplates.getStatisticsCompareLegend('.compareLegend1', 12);
-        if ($('.compareWeaponDescriptionContainer11')) {
-            $('.compareWeaponDescriptionContainer11').remove();
-            $('.statisticsFor11').remove();
-        }
+
         let compareStatistics = new CompareStatistics();
         compareStatistics.compareAllStatistics(weapons, 'dmg', '#damage', colors);
         compareStatistics.compareAllStatistics(weapons, 'muzzleVelocity', '#muzzleVelocity', colors);
@@ -69,86 +85,70 @@ $(document).ready(function () {
         }
     }
 
-    let compareDeleteItem = $('.compareDeleteItem');
-    compareDeleteItem.on('mouseover', function () {
-        let itemId = $(this).attr('id').substr(17, 2);
-        $('.compareWeaponDescriptionContainer' + itemId + ',.statisticsFor' + itemId).css('background-color', 'rgba(255,103,93,0.05)');
-    });
+    // let compareDeleteItem = $('.compareDeleteItem');
+    // compareDeleteItem.on('mouseover', function () {
+    //     let itemId = $(this).attr('id').substr(17, 2);
+    //     $('.compareWeaponDescriptionContainer' + itemId + ',.statisticsFor' + itemId).css('background-color', 'rgba(255,103,93,0.05)');
+    // });
+    //
+    // compareDeleteItem.on('mouseout', function () {
+    //     let itemId = $(this).attr('id').substr(17, 2);
+    //     $('.compareWeaponDescriptionContainer' + itemId + ',.statisticsFor' + itemId).css('background-color', '');
+    // });
 
-    compareDeleteItem.on('mouseout', function () {
-        let itemId = $(this).attr('id').substr(17, 2);
-        $('.compareWeaponDescriptionContainer' + itemId + ',.statisticsFor' + itemId).css('background-color', '');
-    });
-
-    compareDeleteItem.on('click', function (e) {
-        let allContainers = $('div[class *= "compareWeaponDescriptionContainer"]');
-        let targetContainer = e.target.parentElement.parentElement.parentElement;
-        let itemId = $(this).attr('id').substr(17, 2);
-        let i;
-        for (i = 0; i < allContainers.length; i++) {
-            if (targetContainer.getAttribute('class') === allContainers[i].getAttribute('class')) {
-                $('.compareWeaponDescriptionContainer' + itemId + ',.statisticsFor' + itemId).animate({
-                    opacity: 0.1,
-                    bottom: "15rem",
-                }, 300, function () {
-                    this.remove()
-                });
-
-                if (i < 5) {
-                    //TODO dokonczyc przenoszenie elementu
-                    let elemToCut = $('.compareWeaponDesc2 div[class *= "compareWeaponDescriptionContainer"]:first-child');
-                    console.log(elemId.getCla);
-                    let statisticsElemToCut = $('.statisticsFor' + elemId);
-                    elemToCut.remove();
-                    statisticsElemToCut.remove();
-
-                    console.log(' ten element trzeba przeniesc' + elemToCut)
-                }
-
-
-                let weaponsInStorage = localStorage.getItem('weapons');
-                let weapons = JSON.parse(weaponsInStorage);
-                weapons[i + 1] = undefined;
-
-                let weaponsToSave = [];
-                weaponsToSave[0] = undefined;
-                for (let i = 1; i < weapons.length; i++) {
-                    if (weapons[i] !== undefined) {
-                        weaponsToSave.push(weapons[i])
-                    }
-                }
-
-                localStorage.clear();
-                localStorage.setItem('weapons', JSON.stringify(weaponsToSave));
-
-                if (allContainers.length < 7) {
-                    $('.compareLegend2').empty()
-                }
-
-                if (weaponsToSave.length <= 1) {
-                    localStorage.clear();
-                    $('.compareLegend1').empty()
-                }
-            }
-        }
-
-        // if(targetContainer.getAttribute('class') === $("div[class *= 'compareWeaponDescriptionContainer']:eq(0)").attr('class')){
-        //     console.log('===')
-        // }
-        // if ($('.compareWeaponDescriptionContainer1').attr('id') == $('.compareWeaponDescriptionContainer1').attr('id')){
-        //     console.log('test');
-        // }
-        //console.log($("div[class *= 'compareWeaponDescriptionContainer']:eq(0)"));
-
-        // let itemId = $(this).attr('id').substr(17, 2);
-        // $('.compareWeaponDescriptionContainer' + itemId + ',.statisticsFor' + itemId).animate({
-        //     opacity: 0.1,
-        //     bottom:"15rem",
-        // },300, function () {
-        //     this.remove()
-        // });
-
-
-        //location.reload();
-    });
+    // $('.compareWeaponDesc1, .compareWeaponDesc2').on('click','button[id *= "compareDeleteItem"]', function (e) {
+    //
+    //     let allContainers = $('div[class *= "compareWeaponDescriptionContainer"]');
+    //     let targetContainer = e.target.parentElement.parentElement.parentElement;
+    //     let itemId = $(this).attr('id').substr(17, 2);
+    //     let i;
+    //     for (i = 0; i < allContainers.length; i++) {
+    //         if (targetContainer.getAttribute('class') === allContainers[i].getAttribute('class')) {
+    //             $('.compareWeaponDescriptionContainer' + itemId + ',.statisticsFor' + itemId).animate({
+    //                 opacity: 0.1,
+    //                 bottom: "15rem",
+    //             }, 300, function () {
+    //                 this.remove()
+    //             });
+    //             let elemPosition = this.getBoundingClientRect();
+    //
+    //             if (i < 5) {
+    //                 //TODO dokonczyc przenoszenie elementu
+    //                 let descToCut = $('.compareWeaponDesc2 div[class *= "compareWeaponDescriptionContainer"]:first-child');
+    //                 let statisticsElemToCut = $('.compareStatistics2 div[class *= "statisticsFor"]:first-child');
+    //
+    //                 descToCut.remove();
+    //                 statisticsElemToCut.remove();
+    //                 $('.compareWeaponDesc1').append(descToCut);
+    //                 $('.compareStatistics1').append(statisticsElemToCut);
+    //
+    //             }
+    //
+    //
+    //             let weaponsInStorage = localStorage.getItem('weapons');
+    //             let weapons = JSON.parse(weaponsInStorage);
+    //             weapons[i + 1] = undefined;
+    //
+    //             let weaponsToSave = [];
+    //             weaponsToSave[0] = undefined;
+    //             for (let i = 1; i < weapons.length; i++) {
+    //                 if (weapons[i] !== undefined) {
+    //                     weaponsToSave.push(weapons[i])
+    //                 }
+    //             }
+    //
+    //             localStorage.clear();
+    //             localStorage.setItem('weapons', JSON.stringify(weaponsToSave));
+    //
+    //             if (allContainers.length < 7) {
+    //                 $('.compareLegend2').empty()
+    //             }
+    //
+    //             if (weaponsToSave.length <= 1) {
+    //                 localStorage.clear();
+    //                 $('.compareLegend1').empty()
+    //             }
+    //         }
+    //     }
+    // });
 });
