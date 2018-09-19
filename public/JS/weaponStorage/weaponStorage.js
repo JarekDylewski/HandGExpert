87f0051db4cb14ac8e8a618a04dea29efa7df5f9
JarekDylewski <jaroslaw.dylewski@interia.pl>
2018-09-19 23:50:55 +0200

    Logika odpowiedzialna za zapisywanie bronie do weaponSotrage oraz wyswietlanie komunikatów

---
+++ public/JS/weaponStorage/weaponStorage.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    $('#addWeaponToStorage').on('click', function (e) {
+        // e.preventDefault();
+
+        $.ajax({
+            url: $(e.currentTarget).attr('href'),
+            method: "get",
+            dataType: 'json',
+            data: {
+                weaponCategory: modsAndGunSelectedByUser.getWeaponCategory(),
+                gunId: modsAndGunSelectedByUser.getGunId(),
+                ammoId: modsAndGunSelectedByUser.getAmmoId(),
+                crosshairId: modsAndGunSelectedByUser.getCrosshairId(),
+                triggerId: modsAndGunSelectedByUser.getTriggerId(),
+                springId: modsAndGunSelectedByUser.getSpringId(),
+                barrelId: modsAndGunSelectedByUser.getBarrelId()
+            }
+        }).done(function ($data) {
+
+            $('.gun-bar').before(`
+                  <div class="row w-100 position-absolute weaponStorageAlert">
+                        <div style="z-index: 10;" id="weaponStorageMessages" class="col-7 offset-5 alert alert-info border border-info">
+                             
+                        </div>
+                   </div>
+            `);
+
+            let weaponStorage = $data.weaponStorage;
+            let len = weaponStorage.length;
+            for (let i = 0; i <= len; i++) {
+                $('#weaponStorageMessages').append(weaponStorage[i].message + "<br>");
+            }
+
+            setTimeout(function () {
+                $('#weaponStorageMessages').animate({
+                    opacity: 0.1
+                }, 600, function () {
+                    $('.weaponStorageAlert').remove();
+                })
+            }, 5000)
+
+        })
+    });
+});
