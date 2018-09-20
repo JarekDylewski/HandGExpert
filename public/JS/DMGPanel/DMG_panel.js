5d1ad44df6ed2275ed693ece8aad91a90db40b7c
JarekDylewski <jaroslaw.dylewski@interia.pl>
2018-09-20 23:39:47 +0200

    Poprawka obsługi przycisków

--- public/JS/DMGPanel/DMG_panel.js
+++ public/JS/DMGPanel/DMG_panel.js
@@ -1,16 +1,12 @@
 $(document).ready(function () {
     $('.DMG_panel').on('click', function (e) {
-        e.preventDefault();
-        //button cleaning
-        //cant use loop index in nth-child/eq filter
-        if ($('div ul li[class*="list-item-statistics"]:eq(1)').hasClass('btn-statistics-panel')) {
-            $('div ul li[class*="list-item-statistics"]:gt(1)').removeClass('btn-statistics-panel');
-            $('div ul li[class*="list-item-statistics"]:lt(1)').removeClass('btn-statistics-panel');
+
+        let currentTarget = $(e.currentTarget);
+        if (currentTarget.hasClass('btn-statistics-panel')) {
             //do nothing
         } else {
-            $('div ul li[class*="list-item-statistics"]:eq(1)').toggleClass('btn-statistics-panel');
-            $('div ul li[class*="list-item-statistics"]:gt(1)').removeClass('btn-statistics-panel');
-            $('div ul li[class*="list-item-statistics"]:lt(1)').removeClass('btn-statistics-panel');
+            $('div ul li[class*="list-item-statistics"]').removeClass('btn-statistics-panel');
+            $(currentTarget).toggleClass('btn-statistics-panel');
         }
         //dodanie etykiet podpowiedzi
         $(function () {
