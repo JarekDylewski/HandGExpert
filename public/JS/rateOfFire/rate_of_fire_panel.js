82e50558e1920d054e36208a9efe7cd2ddb3e696
JarekDylewski <jaroslaw.dylewski@interia.pl>
2018-09-21 20:22:46 +0200

    Poprawka obsługi przycisków

--- public/JS/rateOfFire/rate_of_fire_panel.js
+++ public/JS/rateOfFire/rate_of_fire_panel.js
@@ -1,16 +1,15 @@
 //TODO uwzględnić czas przeładowania rygla
 $(document).ready(function () {
-    $('.rate_of_fire_panel').on('click', function () {
+    $('.rate_of_fire_panel').on('click', function (e) {
         //button cleaning
-        if ($('div ul li[class*="list-item-statistics"]:eq(3)').hasClass('btn-statistics-panel')) {
-            $('div ul li[class*="list-item-statistics"]:gt(3)').removeClass('btn-statistics-panel');
-            $('div ul li[class*="list-item-statistics"]:lt(3)').removeClass('btn-statistics-panel');
+        let currentTarget = $(e.currentTarget);
+        if (currentTarget.hasClass('btn-statistics-panel')) {
             //do nothing
         } else {
-            $('div ul li[class*="list-item-statistics"]:eq(3)').toggleClass('btn-statistics-panel');
-            $('div ul li[class*="list-item-statistics"]:gt(3)').removeClass('btn-statistics-panel');
-            $('div ul li[class*="list-item-statistics"]:lt(3)').removeClass('btn-statistics-panel');
+            $('div ul li[class*="list-item-statistics"]').removeClass('btn-statistics-panel');
+            $(currentTarget).toggleClass('btn-statistics-panel');
         }
+
         //dodanie etykiet podpowiedzi
         $(function () {
             $('[data-toggle="tooltip"]').tooltip()
