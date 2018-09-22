25986abe20995e729889b83daa7d99ae8428708a
JarekDylewski <jaroslaw.dylewski@interia.pl>
2018-09-22 17:41:45 +0200

    Kopiowanie linku do udostępnienia modyfikacji broni

--- public/JS/weaponStorage/weaponStorage.js
+++ public/JS/weaponStorage/weaponStorage.js
@@ -1,33 +1,25 @@
 $(document).ready(function () {
-    let template = new CompareTemplates();
+    $('div [class *= getShareLink]').on('click', function (e) {
+        let clickedArea = $(e.currentTarget);
+        let shareLink = clickedArea.attr('data-shareLink');
 
+        clickedArea.append(`<input type="text" class="copyPlaceholder position-fixed" value="${shareLink}">`);
+        let el = document.getElementsByClassName('copyPlaceholder');
 
-    $.ajax({
-        url: "/WeaponStorage",
-        method: "get"
-    }).done(function ($data) {
-        let len = $data.storage.length;
-        for (let i = 0; i < len; i++) {
-            let gunId = $data.storage[i].gunId;
-            let gunName = $data.storage[i].gunName;
-            let ammoName = $data.storage[i].ammoName;
-            let crosshairName = $data.storage[i].crosshairName;
-            let triggerName = $data.storage[i].triggerName;
-            let springName = $data.storage[i].springName;
-            let barrelName = $data.storage[i].barrelName;
+        el[0].select();
+        document.execCommand('copy');
+        el[0].parentNode.removeChild(el[0]);
 
-            //TODO przygodowac nowy szablon w oparciu o ponizszy
-            template.getCompare2('.weaponStorageSpace', gunId, 2);
+        clickedArea.prepend(`<span class="alert alert-info border border-info copiedAlert position-absolute mt-2 ml-4">Link copied!</span>`);
 
-            $('img[id *="compareWeaponImg"]:eq(' + i + ')').attr('src', '/img/gunRibbonsByID/' + gunId + '.png');
-            $('span[id *="compareWeaponName"]:eq(' + i + ')').html(gunName);
-            $('div[id *="compareAmmoMod"]:eq(' + i + ')').html(ammoName);
-            $('div[id *="compareCrosshairMod"]:eq(' + i + ')').html(crosshairName);
-            $('div[id *="compareTriggerMod"]:eq(' + i + ')').html(triggerName);
-            $('div[id *="compareSpringMod"]:eq(' + i + ')').html(springName);
-            $('div[id *="compareBarrelMod"]:eq(' + i + ')').html(barrelName);
-            $('#compareAmmoMod' + gunId)
-        }
-        $('div[class *= "compareWeaponDescriptionContainer"]').addClass('mr-4 mb-4');
-    })
+        setTimeout(function () {
+            $('.copiedAlert').animate({
+                opacity: 0.05
+            }, 500, function () {
+                $('.copiedAlert').remove()
+            })
+        }, 700)
+
+    });
+
 });
